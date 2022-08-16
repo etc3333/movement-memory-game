04d473f7453d587c3f95b578fe37ba82af1bab53
Ethan Chen <ethan.chen333@gmail.com>
2022-08-15 19:13:45 -0700

    test

--- src/components/EndingScreen.js
+++ src/components/EndingScreen.js
@@ -4,7 +4,7 @@ export const EndingScreen = () => {
     return (
         <div id="endingScreen-container">
             <div id="endingScreen-centerBox">
-                hello
+                helloe fafa f 
             </div>
         </div>
     )
