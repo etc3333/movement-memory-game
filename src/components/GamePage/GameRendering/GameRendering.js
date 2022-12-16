1043ef84b2fdbcfb2f4f74af03076c22e8b78173
Ethan Chen <ethan.chen333@gmail.com>
2022-12-15 22:09:32 -0800

    save

--- src/components/GamePage/GameRendering/GameRendering.js
+++ src/components/GamePage/GameRendering/GameRendering.js
@@ -40,12 +40,13 @@ export const GameRendering = React.memo(() => {
 
     const circleDataArray = useMemo(() => setCircleDataArray(),[nextLevel]);
     function setCircleDataArray() {
+//rdmNumber(-3,3)
         return new Array(numberOfTotalCircles.current).fill(circleDataTemplate).map(value => {
             return {...value,
                 x: rdmNumber(0, boxDimensions.width - allCircleRadius + 1), 
                 y: rdmNumber(0, boxDimensions.height - allCircleRadius + 1), 
-                xSpeed: rdmNumber(-3,3), 
-                ySpeed: rdmNumber(-3,3)
+                xSpeed: 5, 
+                ySpeed: 5
             }
         });
     }
